62abb5f80630de05a8ca1209b2999076a335aed7
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-06-24 17:52:33 +0200

    Add __init__ to PolicyBase

--- rrc_2022_datasets/policy_base.py
+++ rrc_2022_datasets/policy_base.py
@@ -1,11 +1,19 @@
 import typing
 from abc import ABC, abstractmethod
 
+import gym
 import numpy as np
 
 
 class PolicyBase(ABC):
     """Base class defining interface for policies."""
+
+    def __init__(self, action_space: gym.Space):
+        """
+        Args:
+            action_space:  Action space of the environment.
+        """
+        pass
 
     def reset(self) -> None:
         """Will be called at the beginning of each episode."""
