7211252cfbfb18d2be4923addfebb28e69c7ddb0
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-06-30 15:30:19 +0200

    Don't make sleep dependent on visualisation
    If real_time=True, it should always sleep, no matter if visualisation is
    enabled or not.

--- rrc_2022_datasets/sim_env.py
+++ rrc_2022_datasets/sim_env.py
@@ -217,8 +217,7 @@ class SimTriFingerCubeEnv(gym.Env):
         """Append desired action to queue and wait if real time is enabled."""
 
         t = self.platform.append_desired_action(robot_action)
-        # TODO: shouldn't it always sleep if real_time is true?
-        if self.visualization and self.real_time:
+        if self.real_time:
             sleep(max(0.001 - (time() - self.time_of_last_step), 0.0))
             self.time_of_last_step = time()
         return t
