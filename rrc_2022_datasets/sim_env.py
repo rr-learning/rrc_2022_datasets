5ce84fb32014e169ce4fe25be54273215979dd83
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-07-04 11:09:28 +0200

    Revert "Don't make sleep dependent on visualisation"
    This reverts commit 7211252cfbfb18d2be4923addfebb28e69c7ddb0.

--- rrc_2022_datasets/sim_env.py
+++ rrc_2022_datasets/sim_env.py
@@ -217,7 +217,8 @@ class SimTriFingerCubeEnv(gym.Env):
         """Append desired action to queue and wait if real time is enabled."""
 
         t = self.platform.append_desired_action(robot_action)
-        if self.real_time:
+        # TODO: shouldn't it always sleep if real_time is true?
+        if self.visualization and self.real_time:
             sleep(max(0.001 - (time() - self.time_of_last_step), 0.0))
             self.time_of_last_step = time()
         return t
