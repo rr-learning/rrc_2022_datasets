990515c5aa9ebda13ab6462a75d82dac7692b7dc
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-08-02 15:45:34 +0200

    Add datasets for real-robot stage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 
 ## [Unreleased]
+- Add datasets for the real-robot stage
 
 ## [1.0.1] - 2022-07-04
 ### Changed
--- rrc_2022_datasets/__init__.py
+++ rrc_2022_datasets/__init__.py
@@ -44,6 +44,62 @@ dataset_params = [
     },
     # real robot
     # ==========
+    # real-robot stage/pushing expert
+    {
+        "name": "trifinger-cube-push-real-expert-v0",
+        "dataset_url": "http://robots.real-robot-challenge.com/public/rrc2022/datasets/trifinger-cube-push-real-expert-v0.hdf5",
+        "ref_min_score": 0.0,
+        "ref_max_score": 1.0 * 15000 / 20,
+        "real_robot": True,
+        "trifinger_kwargs": {
+            "episode_length": 750,
+            "difficulty": 1,
+            "keypoint_obs": True,
+            "obs_action_delay": 10,
+        },
+    },
+    # real-robot stage/pushing mixed
+    {
+        "name": "trifinger-cube-push-real-mixed-v0",
+        "dataset_url": "http://robots.real-robot-challenge.com/public/rrc2022/datasets/trifinger-cube-push-real-mixed-v0.hdf5",
+        "ref_min_score": 0.0,
+        "ref_max_score": 1.0 * 15000 / 20,
+        "real_robot": True,
+        "trifinger_kwargs": {
+            "episode_length": 750,
+            "difficulty": 1,
+            "keypoint_obs": True,
+            "obs_action_delay": 10,
+        },
+    },
+    # real-robot stage/lifting expert
+    {
+        "name": "trifinger-cube-lift-real-expert-v0",
+        "dataset_url": "http://robots.real-robot-challenge.com/public/rrc2022/datasets/trifinger-cube-lift-real-expert-v0.hdf5",
+        "ref_min_score": 0.0,
+        "ref_max_score": 1.0 * 30000 / 20,
+        "real_robot": True,
+        "trifinger_kwargs": {
+            "episode_length": 1500,
+            "difficulty": 4,
+            "keypoint_obs": True,
+            "obs_action_delay": 2,
+        },
+    },
+    # real-robot stage/lifting mixed
+    {
+        "name": "trifinger-cube-lift-real-mixed-v0",
+        "dataset_url": "http://robots.real-robot-challenge.com/public/rrc2022/datasets/trifinger-cube-lift-real-mixed-v0.hdf5",
+        "ref_min_score": 0.0,
+        "ref_max_score": 1.0 * 30000 / 20,
+        "real_robot": True,
+        "trifinger_kwargs": {
+            "episode_length": 1500,
+            "difficulty": 4,
+            "keypoint_obs": True,
+            "obs_action_delay": 2,
+        },
+    },
 ]
 
 
